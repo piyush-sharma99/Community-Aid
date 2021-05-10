7bb949de2413116e238832a5487fd9d7bafcbe75
Piyush Sharma <piyush.sharma1078@gmail.com>
2021-05-10 13:22:55 +0100

    finishing changes

---
+++ apikey.js
@@ -0,0 +1,24 @@
+/*
+ *  ClassName: apikey.js
+ *
+ *  Date: 28/03/2021
+ *
+ * @author Piyush Sharma, X17342356
+ *
+ * @reference https://reactnative.dev/docs/activityindicator
+ * @reference https://www.udemy.com/course/react-native-the-practical-guide/
+ * @reference https://www.youtube.com/watch?v=8zT6CYu0iYQ
+ *
+ */
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDGliQfMfKpXUllG5ZPyClJL6C8VgFKXo8",
+  authDomain: "community-aid.firebaseapp.com",
+  databaseURL:
+    "https://community-aid-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "community-aid",
+  storageBucket: "community-aid.appspot.com",
+  messagingSenderId: "529896701339",
+  appId: "1:529896701339:web:1c30369d39f8654b01353e",
+  measurementId: "G-PPD28P0PC6",
+};
